2fd08d566331214d72c0248b15384370e396c061
Shreya792-cpu <shreyar.23.beai@acharya.ac.in>
2024-09-19 10:12:38 +0530

    Add files via upload

---
+++ coverting odd to even.R
@@ -0,0 +1,17 @@
+#11th Sept'2024
+cat("Shreya Verma\n")
+cat("1AY23AI109")
+vec1=c(1,2,4,3,5)
+vec1
+output1=setdiff(vec1,3)
+output1
+output2=setdiff(vec1,c(2,3,4))
+output2
+odd=array(seq(1,10))
+odd
+out3=odd[odd=(odd%%2!=0)]
+out3
+out4=c(out3)+1
+out4
+
+
